a0cf23e9e02d17194c134ff24961f085232f28e2
leafeon-b <grad.f.ishida@gmail.com>
2024-06-26 20:59:43 +0000

    プロジェクト名の変更

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="mb-10 shadow-lg shadow-gray-100">
       <div className="container mx-auto flex max-w-screen-md items-center justify-between px-2 py-3">
         <Link href="/" className="cursor-pointer text-xl font-bold">
-          厨二病メーカー
+          厨二病ジェネレータ
         </Link>
       </div>
     </header>
